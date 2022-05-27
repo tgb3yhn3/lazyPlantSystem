828c26d3ef26eb8a3e0544e90decd23c5711eef2
a22024125 <66326426+tgb3yhn3@users.noreply.github.com>
2022-05-28 03:39:00 +0800

    1st upload
    正在製作前端和測試後端的可行性

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
+app = Flask(__name__)
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '51689883'
+app.config['MYSQL_DB'] = 'sensordata'
+ 
+mysql = MySQL(app)
+
+@app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
+
+if __name__ == '__main__':
+    app.run(debug=True)
